fb2aaa4aeeba2340c5583d0ae5b3759d725db7d3
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-02 04:19:37 -0500

    Fixed Timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,14 @@
-function firstTimer() {
-    const startingSeconds = 4;
-    let time = startingSeconds;
+const startingSeconds = 4;
+let time = startingSeconds;
 
-    const countdownEl = document.getElementById("first");
-    setInterval(updateCountdown, 1000);
+const countdownEl = document.getElementById("first");
+setInterval(updateCountdown, 1000);
 
-    function updateCountdown() {
-        if (time >= 0) {
-            const minutes = Math.floor(time / 60);
-            let seconds = time % 60;
-            countdownEl.innerHTML = `${minutes}: ${seconds}`;
-            time--;
-        }
+function updateCountdown() {
+    if (time >= 0) {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        countdownEl.innerHTML = `${seconds}`;
+        time--;
     }
 }
