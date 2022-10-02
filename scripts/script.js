978a319bac955285944953cc98bde13d0a1b5df6
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-02 04:13:20 -0500

    Added timer div ids

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
-// const startingSeconds = 4;
-// let time = startingSeconds;
+function firstTimer() {
+    const startingSeconds = 4;
+    let time = startingSeconds;
 
-// const countdownEl = document.getElementById("timer");
-// setInterval(updateCountdown, 1000);
+    const countdownEl = document.getElementById("first");
+    setInterval(updateCountdown, 1000);
 
-// function updateCountdown () {
-//     const minutes = Math.floor(time / 60);
-//     let seconds = time % 60;
-//     countdownEl.innerHTML = `${minutes}: ${seconds}`;
-//     time--;
-// }
+    function updateCountdown() {
+        if (time >= 0) {
+            const minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+            countdownEl.innerHTML = `${minutes}: ${seconds}`;
+            time--;
+        }
+    }
+}
