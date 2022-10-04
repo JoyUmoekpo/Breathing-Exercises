087e2b283b1693c9a380d93b59d261bb57d6f4d2
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-03 23:33:36 -0500

    Rewrote "first" timer function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
+function updateCountdown() {
 const startingSeconds = 4;
 let time = startingSeconds;
 
 const countdownEl = document.getElementById("first");
 setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
     if (time >= 0) {
         // const minutes = Math.floor(time / 60);
         let seconds = time % 60;
