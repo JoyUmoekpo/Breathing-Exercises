4b0aace3f98edd572ab80b8cf4e17bee4e2705d2
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-04 20:20:36 -0500

    Wrote 'second timer' function

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,45 @@
+function breatheIn(){
 const timerOneSeconds = 4;
 let timeOne = timerOneSeconds;
 
-const countdownEl = document.getElementById("first");
-setInterval(updateCountdown, 1000);
+const countdownOne = document.getElementById("first");
+setInterval(updateCountdownOne, 1000);
 
-function updateCountdown() {
+function updateCountdownOne() {
 
     
     if (timeOne >= 0) {
         // const minutes = Math.floor(time / 60);
-        let seconds = timeOne % 60;
-        countdownEl.innerHTML = `Breathe in: ${seconds}`;
+        let secondsOne = timeOne % 60;
+        countdownOne.innerHTML = `Breathe in: ${secondsOne}`;
         timeOne--;
+        
+    }
+    
+    if (timeOne == 0) {
+        holdBreath()
     }
 }
+}
+
+function holdBreath() {
+const timerTwoSeconds = 7;
+let timeTwo = timerTwoSeconds;
+
+const countdownTwo = document.getElementById("second");
+setInterval(updateCountdownTwo, 1000);
+
+function updateCountdownTwo() {
+
+    
+    if (timeTwo >= 0) {
+        // const minutes = Math.floor(time / 60);
+        let secondsTwo = timeTwo % 60;
+        countdownTwo.innerHTML = `Hold breath: ${secondsTwo}`;
+        timeTwo--;
+    }
+}
+}
+
+breatheIn();
+// holdBreath();
